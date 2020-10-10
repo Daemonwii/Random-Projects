a170aca2e342cc76da1855441c6706709dad608a
Daemonwii <56087237+Daemonwii@users.noreply.github.com>
2020-10-10 20:12:24 -0300

    Add files via upload

---
+++ RUT Verifier.py
@@ -0,0 +1,19 @@
+RUT = input("Enter RUT:")
+sum = 0
+times = 2
+for largo in str(RUT):
+    if times < 7:
+         sum = sum + ((RUT%10) * times)
+         times = times + 1
+         RUT = RUT // 10
+    if times == 7:
+        sum = sum + ((RUT%10) * times)
+        times = times + 1
+        RUT = RUT // 10
+        times = 2
+verifier = 11 - (sum % 11)
+if verifier == 10:
+    verifier = "K"
+if verifier == 11:
+    verifier = 0
+print("Verifier is " , verifier)
