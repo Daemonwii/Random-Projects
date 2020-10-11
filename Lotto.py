03730180a0ff452b2401a8a1d9170453eccc4752
Daemonwii <56087237+Daemonwii@users.noreply.github.com>
2020-10-11 20:00:09 -0300

    Added currencies

--- Lotto.py
+++ Lotto.py
@@ -4,7 +4,7 @@ while True:
     number = random.randint(0,4496388)
     if number == 1:
         print number
-        print 'After',loop,'attempts, and spending $',(loop*3000),'you finally won the lottery!'
+        print 'After',loop,'attempts, and spending CLP$',(loop*3000),'or about USD$',((loop*3000)/797),'you finally won the lottery!'
         break
     else:
         loop = loop + 1
